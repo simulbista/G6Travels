e4e665abc95a9ec2b80e643f9812af3072772522
Simul Bista <simulbista@gmail.com>
2022-04-11 22:53:55 -0400

    countdown timer on food page using food.js fixed(the value stops at 0 min and 0 sec), email validation added on the form on blog page using js, improved accessibility

--- javascript/blog.js
+++ javascript/blog.js
@@ -18,11 +18,14 @@ let passwordagain = document.getElementById('passwordagain');
 let form = document.getElementById('form');
 let formerror = document.getElementById('formerror');
 
-// prevent submission of forms if there are errors
 form.addEventListener('submit', (e) => {
         let messages = [];
         if (uname.value === '' || uname.value == null) {
             messages.push('Name is required!');
+        }
+
+        if (ValidateEmail(email) == false) {
+            messages.push('Invalid email address');
         }
 
         if (password.value.length < 6) {
@@ -32,10 +35,23 @@ form.addEventListener('submit', (e) => {
         if (password.value !== '' && password.value != passwordagain.value) {
             messages.push("Passwords don't match");
         }
-
+        // prevent submission of forms if there are errors
         if (messages.length > 0) {
             e.preventDefault();
+            // adding new line for each error messages
             formerror.innerText = messages.join('\n');
         }
     })
     // end of form validation
+
+//email validation function
+function ValidateEmail(input) {
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (input.value.match(validRegex)) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
--- javascript/food.js
+++ javascript/food.js
@@ -1,16 +1,13 @@
 // js for a countdown timer
-
 console.log("Javascript timer countdown initiated");
 const startMin = 5;
 let time = startMin * 60;
-
 const countDown = document.getElementById('count-down');
+let counter = 0;
 
 // calling updateCountdown every sec
-setInterval(updateCountdown, 1000);
-
-
-function updateCountdown() {
+// setInterval(updateCountdown, 1000);
+let i = setInterval(function updateCountdown() {
     const min = Math.floor(time / 60);
     let sec = time % 60;
 
@@ -18,7 +15,11 @@ function updateCountdown() {
     sec = sec < 10 ? '0' + sec : sec;
 
     countDown.innerHTML = `${min}:${sec}`;
-
     time--;
 
-}
+    // counter to stop the function execution after the timer reaches 0 mins and 0 seconds
+    counter++;
+    if (counter == 301) {
+        clearInterval(i);
+    }
+}, 1000);
