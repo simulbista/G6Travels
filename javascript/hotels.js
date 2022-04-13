60133a882fb12d54b452f9effb703be6999d32b9
Anna Salas <annasalas999@gmail.com>
2022-04-13 17:46:26 -0400

    link search to book page

--- javascript/hotels.js
+++ javascript/hotels.js
@@ -1,9 +1,13 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "<img src= 'images/foods.jpg' width=\'400px\' height=\'150px\'>";
+    document.getElementById("demo").innerHTML = "<img src= '../images/foods.jpg' width=\'180px\' height=\'100px\'>";
   }
   function myFunction1() {
-    document.getElementById("demo1").innerHTML = "<img src= 'images/refreshments.jpg' width=\'400px\' height=\'150px\'>";
+    document.getElementById("demo1").innerHTML = "<img src= '../images/refreshments.jpg' width=\'180px\' height=\'100px\'>";
   }
   function myFunction2() {
-    document.getElementById("demo2").innerHTML = "<img src= 'images/security.jpg' width=\'400px\' height=\'150px\'>";
+    document.getElementById("demo2").innerHTML = "<img src= '../images/security.jpg' width=\'180px\' height=\'100px\'>";
+  }
+  
+  function Redirect1() {
+    window.location.assign("http://127.0.0.1:5500/pages/bookHotels.html");
   }
