12d5128bc5a4f6d4c6dea62cab7b6cfa2dc4e68f
Simul Bista <simulbista@gmail.com>
2022-04-13 10:51:53 -0400

    changed header section(myprofile and sign in), urls fixes

--- javascript/hotel1.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "<img src= 'images/foods.jpg' width=\'400px\' height=\'150px\'>";
-  }
-  function myFunction1() {
-    document.getElementById("demo1").innerHTML = "<img src= 'images/refreshments.jpg' width=\'400px\' height=\'150px\'>";
-  }
-  function myFunction2() {
-    document.getElementById("demo2").innerHTML = "<img src= 'images/security.jpg' width=\'400px\' height=\'150px\'>";
-  }
---
+++ javascript/hotels.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "<img src= 'images/foods.jpg' width=\'400px\' height=\'150px\'>";
+  }
+  function myFunction1() {
+    document.getElementById("demo1").innerHTML = "<img src= 'images/refreshments.jpg' width=\'400px\' height=\'150px\'>";
+  }
+  function myFunction2() {
+    document.getElementById("demo2").innerHTML = "<img src= 'images/security.jpg' width=\'400px\' height=\'150px\'>";
+  }
